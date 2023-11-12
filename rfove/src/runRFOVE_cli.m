fc831d00740797e00b3903f9afb4ef03f15360c1
Leonid Kostrykin <void@evoid.de>
2023-11-12 21:40:39 +0100

    fixes cli matlab code

--- rfove/src/runRFOVE_cli.m
+++ rfove/src/runRFOVE_cli.m
@@ -1,8 +1,6 @@
-function exitcode = runRFOVE_cli(areaUB, minAreaMaxAreaRatioUB, overlapUB, NeighborhoodSize, filesList)
+function exitcode = runRFOVE_cli(areaUB, minAreaMaxAreaRatioUB, overlapUB, NeighborhoodSize, inputFile, outputFile)
 addpath(genpath('MatlabCentral_IVC2020'));
 
-close all;
-clear all;
 %IMPLEMENTATION RFOVE method [1]
 % [1] C. Panagiotakis and A.A. Argyros, "Region-based Fitting of Overlapping Ellipses and its 
 % Application to Cells Segmentation", Image and Vision Computing, Elsevier, vol. 93, pp. 103810, 2020.
@@ -37,55 +35,12 @@ AICBIC_SELECTION = 1; %Set AICBIC_SELECTION = 1, to use AIC is selected else BIC
 
 set(0,'DefaultFigureColormap',jet);
 
-DataDirD{1} = 'Dataset_BBBC039//images//';
-GTDirD{1} = 'Dataset_BBBC039//outlines//';
-ResultsDirD{1} = 'RES_BBBC039//';
+[I] = imread(inputFile);
+GT = imread(inputFile);
+[ GT ] = correctGT(GT);
 
-filesD{1} = [strcat(filesList, ' ')];
-
-DATASET = 1;
-DataDir = DataDirD{DATASET};
-GTDir = GTDirD{DATASET};
-ResultsDir = ResultsDirD{DATASET};
-
-%RUN WHOLE DATASET
-if exist('MFILE') == 0,
-    files = filesD{DATASET};
-    apo = 1;
-    s = find(isspace(files)==1);
-    data = cell(1,length(s));
-    id = 1;
-    j0 = 1;
-else
-    load(MFILE);
-    j0 = j+1;
-end
-for j=j0:length(s),
-    close all;
-    eos = s(j);
-    fname = files(apo:eos-1);
-    fnameGT = fname;
-    fnameGT(length(fnameGT)) = '1';
-    apo = eos+1;
-    imagePerRUN = j / length(s)
-    fname
-    
-    [I] = imread(sprintf('%s%s.png',DataDir,fname));
-    GT = imread(sprintf('%s%s.png',GTDir,fname));
-    [ GT ] = correctGT( GT);
-    
-    [IClustTotal,totEll,INITSEG] = runMainAlgo(imgaussfilt(I,2),AICBIC_SELECTION,METHOD,METHODSEG,NeighborhoodSize,0.5,0);
-    [REC, PR, F1, Overlap,BP,BDE,RECL,PRL,F1L,LGT] = getInitSegmentationStats(GT,INITSEG,IClustTotal);
-    [Jaccard, MAD, Hausdorff, DiceFP,DiceFN,FP,FN,LGT] =getStats(GT,INITSEG,IClustTotal);
-    imwrite(uint16(IClustTotal),sprintf('%s%s.tiff',ResultsDir,fname))
-    
-    close all;
-    statsE{id} = totEll;
-    Fnames{id} = fname;
-    statsPan(id,1:9) = [REC, PR, F1, Overlap,BP,BDE,RECL,PRL,F1L];
-    stats(id,1:7) = [Jaccard, MAD, Hausdorff, DiceFP,DiceFN,FP,FN];
-    id = id+1;
-    save(sprintf('%sAIC.mat', ResultsDir),'stats','statsE','statsPan','Fnames');
-end
-
+[IClustTotal,totEll,INITSEG] = runMainAlgo(imgaussfilt(I,2),AICBIC_SELECTION,METHOD,METHODSEG,NeighborhoodSize,0.5,0);
+imwrite(uint16(IClustTotal), outputFile)
+ 
+close all;
 exitcode = 0;
